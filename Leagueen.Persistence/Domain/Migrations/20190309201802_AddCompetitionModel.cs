b20b0a1589e04c732a4e46b7bdfc120879d40c2f
Michal Sakwa <saka1988@gmail.com>
2019-03-09 21:19:34 +0100

    Added competition model migration.

---
+++ Leagueen.Persistence/Domain/Migrations/20190309201802_AddCompetitionModel.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Leagueen.Persistence.Domain.Migrations
+{
+    public partial class AddCompetitionModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "leagueen",
+                table: "Competitions",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                schema: "leagueen",
+                table: "Competitions",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Model",
+                schema: "leagueen",
+                table: "Competitions",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model",
+                schema: "leagueen",
+                table: "Competitions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "leagueen",
+                table: "Competitions",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                schema: "leagueen",
+                table: "Competitions",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 16);
+        }
+    }
+}
