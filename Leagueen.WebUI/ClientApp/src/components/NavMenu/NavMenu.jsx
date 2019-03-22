985ebb461364f4fe614da5d1ea6719402dc0de0a
Michal Sakwa <saka1988@gmail.com>
2019-03-23 00:12:36 +0100

    Added navmenu without reactstrap.

--- Leagueen.WebUI/ClientApp/src/components/NavMenu/NavMenu.jsx
+++ Leagueen.WebUI/ClientApp/src/components/NavMenu/NavMenu.jsx
@@ -1,34 +1,27 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavMenu.scss';
 
 function NavMenu(props) {
-  const [collapsed, setCollapsed] = useState(true);
-
-  function toggleNavbar() {
-    setCollapsed(!collapsed);
-  }
 
   return (
-      <nav className="navbar navbar-light navbar-expand-md navbar-toggleable-md ng-white border-bottom box-shadow mb-3">
-        <div className="container-fluid">
+    <nav className="navbar navbar-toggleable-xl border-bottom box-shadow mb-3 ng-white navbar-light" role="navigation">
+      <div className="container-fluid">
+        <div className="navbar-header navbar-left pull-left">
           <button className="mr-2 navbar-toggler" type="button" onClick={props.toggleSidebar}>
             <span className="navbar-toggler-icon"></span>
           </button>
-          <NavbarBrand tag={Link} to="/">Leagueen</NavbarBrand>
-          <Collapse className="d-md-inline-flex flex-md-row-reverse" isOpen={!collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-            </ul>
-          </Collapse>
         </div>
-      </nav>
+        <ul className="navbar-nav flex-row float-right">
+          <li className="nav-item">
+            <span className="navbar-text">Welcome, <strong>Guest</strong>!</span>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/home"> Login</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
