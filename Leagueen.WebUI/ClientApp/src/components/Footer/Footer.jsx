26d21980970c0eb904e4cba8731a37c33790b54c
Michal Sakwa <saka1988@gmail.com>
2019-03-23 01:30:50 +0100

    Remove fixed property from footer.

--- Leagueen.WebUI/ClientApp/src/components/Footer/Footer.jsx
+++ Leagueen.WebUI/ClientApp/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.scss";
 function Footer(props) {
 
     return (
-        <nav className="navbar fixed-bottom box-shadow navbar-dark bg-dark" role="navigation">
+        <nav className="navbar bottom box-shadow navbar-dark bg-dark" role="navigation">
             <footer className="container-fluid">
             </footer>
         </nav>
--- Leagueen.WebUI/ClientApp/src/components/Sidebar/Sidebar.jsx
+++ Leagueen.WebUI/ClientApp/src/components/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import "./Sidebar.scss";
 function Sidebar(props) {
 
     return (
-        <div className="bg-light border-right" id="sidebar-wrapper">
+        <div className="bg-light" id="sidebar-wrapper">
             <div className="sidebar-heading">Leagueen</div>
             <div className="list-group list-group-flush">
                 <Link className="list-group-item list-group-item-action bg-light nav-link" to="/home" >Home</Link>
