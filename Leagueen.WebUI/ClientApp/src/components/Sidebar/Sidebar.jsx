da210efda422f03d915ac877bae10a0fd2ed48a6
Michal Sakwa <saka1988@gmail.com>
2019-03-24 00:07:25 +0100

    Changed sidebar colors. Added active route highlight to sidebar.

--- Leagueen.WebUI/ClientApp/src/components/Sidebar/Sidebar.jsx
+++ Leagueen.WebUI/ClientApp/src/components/Sidebar/Sidebar.jsx
@@ -2,28 +2,45 @@ import React from "react";
 import { RedirectLink } from "components/RedirectLink/RedirectLink";
 import "./Sidebar.scss";
 
+import appRoutes from "routes/app";
+
 function Sidebar(props) {
 
     function setActivity() {
-        if(props.showSidebar)
+        if (props.showSidebar)
             return "active";
         return "";
     };
 
+    function linkClass(prop) {
+        if(prop.path === props.location.pathname)
+            return "btn btn-primary active"
+
+        return "btn btn-primary";
+    };
+
+    function renderLink(prop, key) {
+
+        return (
+            <li key={key}>
+                <RedirectLink className={linkClass(prop)} name={prop.name} to={prop.path}></RedirectLink>
+            </li>
+        );
+    };
+
     return (
         <div className={setActivity()} id="sidebar">
-            <div id="dismiss" onClick={props.toggleSidebar}>
+            <button className="btn btn-primary-dark" id="dismiss" onClick={props.toggleSidebar}>
                 <i className="fas fa-arrow-left"></i>
-            </div>
+            </button>
             <div className="sidebar-header"><h3>Leagueen</h3></div>
 
-            <ul className="list-unstyled components">
-                <li>
-                    <RedirectLink className="btn btn-primary" name="Home" to="/home"></RedirectLink>
-                </li>
-                <li>
-                    <RedirectLink name="Counter" to="/counter"></RedirectLink>
-                </li>
+            <ul className="list-unstyled components justify-content-center">
+                {appRoutes.map((prop, key) => {
+                    if (prop.redirect) return null;
+
+                    return renderLink(prop, key);
+                })}
             </ul>
         </div>
     );
