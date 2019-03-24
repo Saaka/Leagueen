2ecb9df31663818d5154905b7f30e294e33f0c55
Michal Sakwa <saka1988@gmail.com>
2019-03-24 23:21:45 +0100

    Added logout styling.

--- Leagueen.WebUI/ClientApp/src/views/Logout/Logout.jsx
+++ Leagueen.WebUI/ClientApp/src/views/Logout/Logout.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Icon } from "components/Icon/Icon";
 import "./Logout.scss";
 
 function Logout(props) {
 
+    useEffect(() => {
+        
+    });
+
     return (
-        <div className="container h-100 d-flex justify-content-center">
+        <div className="h-100 d-flex justify-content-center logout-content">
             <div className="auto-center">
-                <h1 className="logout-spinner"><Icon icon="spinner" spin /></h1>
+                <div className="bottom-offset">
+                    <h1>Logging out</h1>
+                    <h1><Icon icon="spinner" spin /></h1>
+                </div>
             </div>
         </div>
     );
