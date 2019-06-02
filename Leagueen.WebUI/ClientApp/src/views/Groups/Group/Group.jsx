99997fdb1e126ae8be3578093df9e0b80c2028e7
Michal Sakwa <saka1988@gmail.com>
2019-06-02 16:06:20 +0200

    Added button to close Group component.

--- Leagueen.WebUI/ClientApp/src/views/Groups/Group/Group.jsx
+++ Leagueen.WebUI/ClientApp/src/views/Groups/Group/Group.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Icon } from "components/common";
+import { RouteNames } from "routes/names";
 import "./Group.scss";
 
 export function Group(props) {
@@ -11,10 +12,17 @@ export function Group(props) {
 
     };
 
+    function close() {
+        props.history.push(RouteNames.UserGroups);
+    }
+
     function renderView() {
         return (
             <div>
                 <h5 className="display-5 group-title">Group: {id} <Icon icon="users" /></h5>
+                <div>
+                    <button className="btn btn-secondary" type="button" onClick={(ev) => close(ev)}>Close</button>
+                </div>
             </div>
         )
     }
