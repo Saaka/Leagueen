0e435e7bab1cd0bc6c5f41449fe6ad393832b337
Michal Sakwa <saka1988@gmail.com>
2019-06-05 16:12:34 +0200

    Added unit tests project. Added friendship request tests.

---
+++ Leagueen.Tests.Unit/Domain/Entities/FriendshipRequestShould.cs
@@ -0,0 +1,59 @@
+﻿using Leagueen.Domain.Entities;
+using Leagueen.Domain.Enums;
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace Leagueen.Tests.Unit.Domain.Entities
+{
+    [Trait(Traits.Category, Traits.UnitTests)]
+    [Trait(Traits.UnitTests, nameof(FriendshipRequestShould))]
+    public class FriendshipRequestShould
+    {
+        [Fact]
+        public void HaveValidStateOnCreation()
+        {
+            var guid = "12345";
+            var requesterId = 1;
+            var addresseeId = 2;
+            var createDate = new DateTime(2019, 05, 06, 15, 00, 00);
+
+            var request = new FriendshipRequest(guid, requesterId, addresseeId, createDate);
+            request.Guid.Should().Be(guid);
+            request.Status.Should().Be(FriendshipRequestStatus.Pending);
+            request.RequesterId.Should().Be(requesterId);
+            request.AddresseeId.Should().Be(addresseeId);
+            request.CreateDate.Should().Be(createDate);
+        }
+
+        [Fact]
+        public void HaveAcceptedStatusAfterAccepting()
+        {
+            var guid = "12345";
+            var requesterId = 1;
+            var addresseeId = 2;
+            var createDate = new DateTime(2019, 05, 06, 15, 00, 00);
+
+            var request = new FriendshipRequest(guid, requesterId, addresseeId, createDate)
+                .AcceptRequest();
+
+            request
+                .Status.Should().Be(FriendshipRequestStatus.Accepted);
+        }
+
+        [Fact]
+        public void HaveRejectedStatusAfterRejecting()
+        {
+            var guid = "12345";
+            var requesterId = 1;
+            var addresseeId = 2;
+            var createDate = new DateTime(2019, 05, 06, 15, 00, 00);
+
+            var request = new FriendshipRequest(guid, requesterId, addresseeId, createDate)
+                .RejectRequest();
+
+            request
+                .Status.Should().Be(FriendshipRequestStatus.Rejected);
+        }
+    }
+}
---
+++ Leagueen.Tests.Unit/Traits.cs
@@ -0,0 +1,9 @@
+﻿namespace Leagueen.Tests.Unit
+{
+    class Traits
+    {
+        public const string Category = nameof(Category);
+
+        public const string UnitTests = nameof(UnitTests);
+     }
+}
