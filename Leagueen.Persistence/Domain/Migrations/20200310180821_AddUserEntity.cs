fbb2a06e33a73912810857fe6b430263ec2a62fe
Michal Sakwa <saka1988@gmail.com>
2020-03-10 19:08:51 +0100

    Added users migration

---
+++ Leagueen.Persistence/Domain/Migrations/20200310180821_AddUserEntity.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Leagueen.Persistence.Domain.Migrations
+{
+    public partial class AddUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "leagueen",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserGuid = table.Column<string>(maxLength: 32, nullable: false),
+                    Email = table.Column<string>(maxLength: 256, nullable: false),
+                    DisplayName = table.Column<string>(maxLength: 128, nullable: false),
+                    ImageUrl = table.Column<string>(maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserGuid",
+                schema: "leagueen",
+                table: "Users",
+                column: "UserGuid",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "leagueen");
+        }
+    }
+}
