386583a36179023a8de29f26b8adaceec3cacec7
Michal Sakwa <saka1988@gmail.com>
2020-03-14 17:18:03 +0100

    Added migration to change strings to guids.

---
+++ Leagueen.Persistence/Domain/Migrations/20200314161544_AlterTablesAddGuidsInsteadOfStrings.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Leagueen.Persistence.Domain.Migrations
+{
+    public partial class AlterTablesAddGuidsInsteadOfStrings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserGuid",
+                schema: "leagueen",
+                table: "Users",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "GroupGuid",
+                schema: "leagueen",
+                table: "Groups",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FriendshipRequestGuid",
+                schema: "leagueen",
+                table: "FriendshipRequests",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserGuid",
+                schema: "leagueen",
+                table: "Users",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupGuid",
+                schema: "leagueen",
+                table: "Groups",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FriendshipRequestGuid",
+                schema: "leagueen",
+                table: "FriendshipRequests",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(Guid));
+        }
+    }
+}
